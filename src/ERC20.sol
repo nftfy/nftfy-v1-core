39133ea79d2e57e70f572793a44bb03d1b1140c3
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2020-05-04 15:16:52 -0300

    Adds metadata interfaces for ERC-20 and ERC-721

--- src/ERC20.sol
+++ src/ERC20.sol
@@ -4,10 +4,6 @@ pragma solidity >= 0.4.20;
 
 interface ERC20
 {
-	// Optional
-	// function name() external view returns (string _name);
-	// function symbol() external view returns (string _symbol);
-	// function decimals() external view returns (uint8 _decimals);
 	function totalSupply() external view returns (uint256 _totalSupply);
 	function balanceOf(address _owner) external view returns (uint256 _balance);
 	function transfer(address _to, uint256 _value) external returns (bool _success);
@@ -16,4 +12,11 @@ interface ERC20
 	function allowance(address _owner, address _spender) external view returns (uint256 _remaining);
 	event Transfer(address indexed _from, address indexed _to, uint256 _value);
 	event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+}
+
+interface ERC20Metadata
+{
+	function name() external view returns (string memory _name);
+	function symbol() external view returns (string memory _symbol);
+	function decimals() external view returns (uint8 _decimals);
 }
--- src/ERC721.sol
+++ src/ERC721.sol
@@ -4,10 +4,6 @@ pragma solidity >= 0.4.20;
 
 interface ERC721
 {
-	// Optional
-	// function name() external view returns (string _name);
-	// function symbol() external view returns (string _symbol);
-	// function tokenURI(uint256 _tokenId) external view returns (string _tokenURI);
 	function balanceOf(address _owner) external view returns (uint256 _balance);
 	function ownerOf(uint256 _tokenId) external view returns (address _owner);
 	function safeTransferFrom(address _from, address _to, uint256 _tokenId, bytes calldata _data) external payable;
@@ -20,6 +16,13 @@ interface ERC721
 	event Transfer(address indexed _from, address indexed _to, uint256 indexed _tokenId);
 	event Approval(address indexed _owner, address indexed _approved, uint256 indexed _tokenId);
 	event ApprovalForAll(address indexed _owner, address indexed _operator, bool _approved);
+}
+
+interface ERC721Metadata
+{
+	function name() external view returns (string memory _name);
+	function symbol() external view returns (string memory _symbol);
+	function tokenURI(uint256 _tokenId) external view returns (string memory _tokenURI);
 }
 
 interface ERC721Receiver
