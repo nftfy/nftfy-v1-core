3bf15dbf7a7ec2c544dbe237546a495180cf4bef
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2020-05-04 12:30:07 -0300

    Reviews ERC-721 interface

--- src/ERC721.sol
+++ src/ERC721.sol
@@ -2,16 +2,21 @@
 // https://eips.ethereum.org/EIPS/eip-721
 pragma solidity >= 0.4.20;
 
-interface ERC721 {
-	function balanceOf(address _owner) external view returns (uint256);
-	function ownerOf(uint256 _tokenId) external view returns (address);
-	function safeTransferFrom(address _from, address _to, uint256 _tokenId, bytes calldata data) external payable;
+interface ERC721
+{
+	// Optional
+	// function name() external view returns (string _name);
+	// function symbol() external view returns (string _symbol);
+	// function tokenURI(uint256 _tokenId) external view returns (string _tokenURI);
+	function balanceOf(address _owner) external view returns (uint256 _balance);
+	function ownerOf(uint256 _tokenId) external view returns (address _owner);
+	function safeTransferFrom(address _from, address _to, uint256 _tokenId, bytes calldata _data) external payable;
 	function safeTransferFrom(address _from, address _to, uint256 _tokenId) external payable;
 	function transferFrom(address _from, address _to, uint256 _tokenId) external payable;
 	function approve(address _approved, uint256 _tokenId) external payable;
 	function setApprovalForAll(address _operator, bool _approved) external;
-	function getApproved(uint256 _tokenId) external view returns (address);
-	function isApprovedForAll(address _owner, address _operator) external view returns (bool);
+	function getApproved(uint256 _tokenId) external view returns (address _approved);
+	function isApprovedForAll(address _owner, address _operator) external view returns (bool _approved);
 	event Transfer(address indexed _from, address indexed _to, uint256 indexed _tokenId);
 	event Approval(address indexed _owner, address indexed _approved, uint256 indexed _tokenId);
 	event ApprovalForAll(address indexed _owner, address indexed _operator, bool _approved);
