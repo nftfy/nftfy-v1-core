ad890875b29bb2fd79819d978bb53ccda58f5cd3
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2020-05-15 20:59:04 -0300

    Adds a test contract for the ERC-721 standard

---
+++ src/Blockies.sol
@@ -0,0 +1,34 @@
+// Blockies
+pragma solidity >= 0.4.20;
+
+import "./ERC721Base.sol";
+
+contract Blockies is ERC721Metadata, ERC721Base
+{
+	string constant BASE_URL = "https://blockie.cc";
+
+	function name() public view returns (string memory _name)
+	{
+		return "Blockies V1";
+	}
+
+	function symbol() public view returns (string memory _symbol)
+	{
+		return "KIE";
+	}
+
+	function tokenURI(uint256 _tokenId) public view returns (string memory _tokenURI)
+	{
+		return string(abi.encodePacked(BASE_URL, "/address/0x", bytes32(_tokenId)));
+	}
+
+	constructor (address _owner) public
+	{
+		uint256 _tokenId = uint256(_owner);
+		assert(balances[_owner] + 1 > balances[_owner]);
+		balances[_owner]++;
+		require(owners[_tokenId] == address(0));
+		owners[_tokenId] = _owner;
+	}
+}
+
