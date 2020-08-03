e9cbb84286f460bfd64648113893331f99946732
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2020-08-03 20:39:59 -0300

    Removes gas and gas limit details from truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -17,32 +17,22 @@ module.exports = {
     mainnet: {
       network_id: 1,
       provider: () => new HDWalletProvider(privateKey, 'https://mainnet.infura.io/v3/' + infuraProjectId),
-      gas: 12500000,
-      gasPrice: 80e9,
     },
     ropsten: {
       network_id: 3,
       provider: () => new HDWalletProvider(privateKey, 'https://ropsten.infura.io/v3/' + infuraProjectId),
-      gas: 8000000,
-      gasPrice: 50e9,
     },
     rinkeby: {
       network_id: 4,
       provider: () => new HDWalletProvider(privateKey, 'https://rinkeby.infura.io/v3/' + infuraProjectId),
-      gas: 10000000,
-      gasPrice: 1e9,
     },
     kovan: {
       network_id: 42,
       provider: () => new HDWalletProvider(privateKey, 'https://kovan.infura.io/v3/' + infuraProjectId),
-      gas: 12500000,
-      gasPrice: 100e9,
     },
     goerli: {
       network_id: 5,
       provider: () => new HDWalletProvider(privateKey, 'https://goerli.infura.io/v3/' + infuraProjectId),
-      gas: 8000000,
-      gasPrice: 10e9,
     },
   }
 };
