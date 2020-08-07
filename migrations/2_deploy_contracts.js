f7a33dacbdaf43c4b06656f2ce90b6c091640ae8
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2020-08-06 21:53:26 -0300

    Adds SafeERC721Metadata to contract migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,13 @@
 const Nftfy = artifacts.require('Nftfy');
 const Wrapper = artifacts.require('Wrapper');
 const Shares = artifacts.require('Shares');
+const SafeERC721Metadata = artifacts.require('SafeERC721Metadata');
 
 module.exports = function(deployer) {
+  deployer.deploy(SafeERC721Metadata);
+  deployer.link(SafeERC721Metadata, Wrapper);
   deployer.deploy(Wrapper);
+  deployer.link(SafeERC721Metadata, Shares);
   deployer.deploy(Shares);
   deployer.link(Wrapper, Nftfy);
   deployer.link(Shares, Nftfy);
