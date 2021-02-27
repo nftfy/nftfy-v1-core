b50d5c67a1e7ca52323181f085259cfef94414cb
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2021-02-27 18:45:54 -0300

    Updates repo and adds BSC support

--- README.md
+++ README.md
@@ -11,5 +11,8 @@ Core smart contracts of Nftfy: an ERC-721/ERC-20 NFT Securitizer.
 | rinkeby | [0x727638740980aA0aA0B346d02dd91120Eaac75ed](https://rinkeby.etherscan.io/address/0x727638740980aA0aA0B346d02dd91120Eaac75ed) |
 | kovan   | [0x727638740980aA0aA0B346d02dd91120Eaac75ed](https://kovan.etherscan.io/address/0x727638740980aA0aA0B346d02dd91120Eaac75ed)   |
 | goerli  | [0x727638740980aA0aA0B346d02dd91120Eaac75ed](https://goerli.etherscan.io/address/0x727638740980aA0aA0B346d02dd91120Eaac75ed)  |
+| ------- | ----------------------------------------------------------------------------------------------------------------------------- |
+| bscmain | [0x727638740980aA0aA0B346d02dd91120Eaac75ed](https://bscscan.io/address/0x727638740980aA0aA0B346d02dd91120Eaac75ed)           |
+| bsctest | [0x727638740980aA0aA0B346d02dd91120Eaac75ed](https://testnet.bscscan.io/address/0x727638740980aA0aA0B346d02dd91120Eaac75ed)   |
 
 To obtain test ERC-721 tokens please use the [Blockies](https://blockies.tk) faucet.
--- truffle-config.js
+++ truffle-config.js
@@ -16,26 +16,35 @@ module.exports = {
   networks: {
     mainnet: {
       network_id: 1,
-      provider: () => new HDWalletProvider(privateKey, 'wss://mainnet.infura.io/ws/v3/' + infuraProjectId),
+      provider: () => new HDWalletProvider(privateKey, 'https://mainnet.infura.io/v3/' + infuraProjectId),
     },
     ropsten: {
       network_id: 3,
-      provider: () => new HDWalletProvider(privateKey, 'wss://ropsten.infura.io/ws/v3/' + infuraProjectId),
+      provider: () => new HDWalletProvider(privateKey, 'https://ropsten.infura.io/v3/' + infuraProjectId),
       skipDryRun: true,
     },
     rinkeby: {
       network_id: 4,
-      provider: () => new HDWalletProvider(privateKey, 'wss://rinkeby.infura.io/ws/v3/' + infuraProjectId),
+      provider: () => new HDWalletProvider(privateKey, 'https://rinkeby.infura.io/v3/' + infuraProjectId),
       skipDryRun: true,
     },
     kovan: {
       network_id: 42,
-      provider: () => new HDWalletProvider(privateKey, 'wss://kovan.infura.io/ws/v3/' + infuraProjectId),
+      provider: () => new HDWalletProvider(privateKey, 'https://kovan.infura.io/v3/' + infuraProjectId),
       skipDryRun: true,
     },
     goerli: {
       network_id: 5,
-      provider: () => new HDWalletProvider(privateKey, 'wss://goerli.infura.io/ws/v3/' + infuraProjectId),
+      provider: () => new HDWalletProvider(privateKey, 'https://goerli.infura.io/v3/' + infuraProjectId),
+      skipDryRun: true,
+    },
+    bscmain: {
+      network_id: 56,
+      provider: () => new HDWalletProvider(privateKey, 'https://bsc-dataseed.binance.org/'),
+    },
+    bsctest: {
+      network_id: 97,
+      provider: () => new HDWalletProvider(privateKey, 'https://data-seed-prebsc-1-s1.binance.org:8545/'),
       skipDryRun: true,
     },
   }
