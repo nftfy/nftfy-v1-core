79db8b7f0e6b917574bd220f982e64b92fac987b
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2021-08-05 20:32:39 -0300

    Adds cost optimized version of the fractionalizer

---
+++ contracts/Fraction.sol
@@ -0,0 +1,179 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity ^0.6.0;
+
+import { Initializable } from "@openzeppelin/contracts/proxy/Initializable.sol";
+import { Proxy } from "@openzeppelin/contracts/proxy/Proxy.sol";
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import { ERC721Holder } from "@openzeppelin/contracts/token/ERC721/ERC721Holder.sol";
+import { IERC721 } from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import { IERC721Metadata } from "@openzeppelin/contracts/token/ERC721/IERC721Metadata.sol";
+import { Create2 } from "@openzeppelin/contracts/utils/Create2.sol";
+import { ReentrancyGuard } from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import { Strings } from "@openzeppelin/contracts/utils/Strings.sol";
+
+import { SafeERC721Metadata } from "./SafeERC721Metadata.sol";
+
+library SafeERC721
+{
+	function safeApprove(IERC721 _token, address _to, uint256 _tokenId) internal
+	{
+		try IERC721(_token).approve(_to, _tokenId) {
+		} catch (bytes memory /* _data */) {
+		}
+	}
+}
+
+contract Fractionalizer is ReentrancyGuard
+{
+	mapping (address => mapping (uint256 => uint256)) private nonce;
+
+	function securitize(address _target, uint256 _tokenId, uint256 _sharesCount, uint8 _decimals, uint256 _exitPrice, address _paymentToken, bool _remnant) external nonReentrant
+	{
+		address _from = msg.sender;
+		require(_exitPrice > 0, "invalid exit price");
+		require(_sharesCount > 0, "invalid shares count");
+		require(_exitPrice % _sharesCount == 0, "fractional price per share");
+		require(!_remnant, "unsupported");
+		uint256 _nonce = nonce[_target][_tokenId]++;
+		uint256 _sharePrice = _exitPrice / _sharesCount;
+		bytes memory _params = abi.encode(_target, _tokenId, _nonce, _chainId());
+		bytes memory _bytecode = abi.encodePacked(type(Fraction).creationCode);
+		address _fractions = Create2.deploy(0, keccak256(_params), _bytecode);
+		FractionImpl(_fractions).initialize(_target, _tokenId, _from, _sharesCount, _decimals, _sharePrice, _paymentToken);
+		IERC721(_target).transferFrom(_from, _fractions, _tokenId);
+	}
+
+	function _chainId() internal pure returns (uint256 _chainid)
+	{
+		assembly { _chainid := chainid() }
+		return _chainid;
+	}
+}
+
+contract Fraction is Proxy
+{
+	function _implementation() internal view override returns (address _impl)
+	{
+		return address(0); // replace by FractionImpl address
+	}
+}
+
+contract FractionImpl is Initializable, ERC721Holder, ERC20, ReentrancyGuard
+{
+	using SafeERC20 for IERC20;
+	using SafeERC721 for IERC721;
+	using Strings for uint256;
+	using SafeERC721Metadata for IERC721Metadata;
+
+	address public target;
+	uint256 public tokenId;
+	uint256 public sharesCount;
+	uint256 public sharePrice;
+	address public paymentToken;
+
+	bool public released;
+
+	constructor () ERC20("", "") public {}
+
+	function __name() public view /*override*/ returns (string memory _name) // change ERC20 name() to virtual on deploy
+	{
+		return string(abi.encodePacked(IERC721Metadata(target).safeName(), " #", tokenId.toString(), " Shares"));
+	}
+
+	function __symbol() public view /*override*/ returns (string memory _symbol) // change ERC20 name() to virtual on deploy
+	{
+		return string(abi.encodePacked(IERC721Metadata(target).safeSymbol(), tokenId.toString()));
+	}
+
+	function initialize(address _target, uint256 _tokenId, address _from, uint256 _sharesCount, uint8 _decimals, uint256 _sharePrice, address _paymentToken) external initializer
+	{
+		target = _target;
+		tokenId = _tokenId;
+		sharesCount = _sharesCount;
+		sharePrice = _sharePrice;
+		paymentToken = _paymentToken;
+		released = false;
+		_setupDecimals(_decimals);
+		_mint(_from, _sharesCount);
+		emit Securitize(_from, _target, _tokenId, address(this));
+	}
+
+	function exitPrice() public view returns (uint256 _exitPrice)
+	{
+		return sharesCount * sharePrice;
+	}
+
+	function redeemAmountOf(address _from) public view returns (uint256 _redeemAmount)
+	{
+		require(!released, "token already redeemed");
+		uint256 _sharesCount = balanceOf(_from);
+		uint256 _exitPrice = exitPrice();
+		return _exitPrice - _sharesCount * sharePrice;
+	}
+
+	function vaultBalance() external view returns (uint256 _vaultBalance)
+	{
+		if (!released) return 0;
+		uint256 _sharesCount = totalSupply();
+		return _sharesCount * sharePrice;
+	}
+
+	function vaultBalanceOf(address _from) public view returns (uint256 _vaultBalanceOf)
+	{
+		if (!released) return 0;
+		uint256 _sharesCount = balanceOf(_from);
+		return _sharesCount * sharePrice;
+	}
+
+	function redeem() external payable nonReentrant
+	{
+		require(!released, "token already redeemed");
+		address payable _from = msg.sender;
+		uint256 _paymentAmount = msg.value;
+		uint256 _sharesCount = balanceOf(_from);
+		uint256 _redeemAmount = redeemAmountOf(_from);
+		if (paymentToken == address(0)) {
+			require(_paymentAmount >= _redeemAmount, "insufficient payment amount");
+			uint256 _changeAmount = _paymentAmount - _redeemAmount;
+			if (_changeAmount > 0) _from.transfer(_changeAmount);
+		} else {
+			if (_paymentAmount > 0) _from.transfer(_paymentAmount);
+			if (_redeemAmount > 0) IERC20(paymentToken).safeTransferFrom(_from, address(this), _redeemAmount);
+		}
+		released = true;
+		if (_sharesCount > 0) _burn(_from, _sharesCount);
+		IERC721(target).safeApprove(address(this), tokenId);
+		IERC721(target).transferFrom(address(this), _from, tokenId);
+		emit Redeem(_from, target, tokenId, address(this));
+		_cleanup();
+	}
+
+	function claim() external nonReentrant
+	{
+		require(released, "token not redeemed");
+		address payable _from = msg.sender;
+		uint256 _sharesCount = balanceOf(_from);
+		require(_sharesCount > 0, "nothing to claim");
+		uint256 _claimAmount = vaultBalanceOf(_from);
+		assert(_claimAmount > 0);
+		_burn(_from, _sharesCount);
+		if (paymentToken == address(0)) _from.transfer(_claimAmount);
+		else IERC20(paymentToken).safeTransfer(_from, _claimAmount);
+		emit Claim(_from, target, tokenId, address(this), _sharesCount);
+		_cleanup();
+	}
+
+	function _cleanup() internal
+	{
+		uint256 _sharesLeft = totalSupply();
+		if (_sharesLeft == 0) {
+			selfdestruct(address(0));
+		}
+	}
+
+	event Securitize(address indexed _from, address indexed _target, uint256 indexed _tokenId, address _shares);
+	event Redeem(address indexed _from, address indexed _target, uint256 indexed _tokenId, address _shares);
+	event Claim(address indexed _from, address indexed _target, uint256 indexed _tokenId, address _shares, uint256 _sharesCount);
+}
