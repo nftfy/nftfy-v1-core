03d525e9684e32626b4d2b423a515fc230f2c37d
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2021-09-03 13:12:05 -0300

    Reviews fractional.art wrapper

--- contracts/v1.5/FractionalArtFractionalizer.sol
+++ contracts/v1.5/FractionalArtFractionalizer.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.6.0;
 
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import { IERC721 } from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import { ReentrancyGuard } from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import { SafeMath } from "@openzeppelin/contracts/math/SafeMath.sol";
@@ -13,28 +15,36 @@ interface FractionalArtERC721VaultFactory
 	function mint(string memory _name, string memory _symbol, address _token, uint256 _id, uint256 _supply, uint256 _listPrice, uint256 _fee) external returns (uint256 _vaultNumber);
 }
 
+interface FractionalArtERC721Vault
+{
+	function updateCurator(address _curator) external;
+	function updateAuctionLength(uint256 _length) external;
+}
+
 contract FractionalArtFractionalizer is ReentrancyGuard
 {
+	using SafeERC20 for IERC20;
 	using SafeMath for uint256;
 
 	// Mainnet: 0x85Aa7f78BdB2DE8F3e0c0010d99AD5853fFcfC63
 	// Rinkeby: 0x458556c097251f52ca89cB81316B4113aC734BD1
-	address immutable _vaultFactory;
+	address immutable vaultFactory;
 
-	constructor (address _vaultfactory) public
+	constructor (address _vaultFactory) public
 	{
-		vaultFractory = _vaultfactory;
+		vaultFactory = _vaultFactory;
 	}
 
-	function fractionalize(address _target, uint256 _tokenId, string memory _name, string memory _symbol, uint8 _decimals, uint256 _fractionsCount, uint256 _fractionPrice) external nonReentrant returns (address _fractions)
+	function fractionalize(address _target, uint256 _tokenId, string memory _name, string memory _symbol, uint256 _fractionsCount, uint256 _fractionPrice, uint256 _duration, uint256 _fee) external nonReentrant returns (address _fractions)
 	{
 		address _from = msg.sender;
-		uint256 _listPrice = _fractionsCount.mul(_fractionPrice);
-		uint256 _supply = _fractionsCount.mul(10 ** (uint256(18).sub(_decimals)));
 		IERC721(_target).transferFrom(_from, address(this), _tokenId);
-		IERC721(_target).approve(vaultFractory, _tokenId);
-		uint256 _vaultNumber = FractionalArtERC721VaultFactory(vaultFractory).mint(_name, _symbol, _target, _tokenId, _supply, _listPrice, 0);
-		_fractions = FractionalArtERC721VaultFactory(vaultFractory).vaults(_vaultNumber);
+		IERC721(_target).approve(vaultFactory, _tokenId);
+		uint256 _vaultNumber = FractionalArtERC721VaultFactory(vaultFactory).mint(_name, _symbol, _target, _tokenId, _fractionsCount, _fractionsCount.mul(_fractionPrice), _fee / 1e15);
+		_fractions = FractionalArtERC721VaultFactory(vaultFactory).vaults(_vaultNumber);
+		FractionalArtERC721Vault(_fractions).updateAuctionLength(_duration);
+		FractionalArtERC721Vault(_fractions).updateCurator(_from);
+		IERC20(_fractions).safeTransfer(_from, IERC20(_fractions).balanceOf(address(this)));
 		emit Fractionalize(_from, _target, _tokenId, _fractions);
 		return _fractions;
 	}
