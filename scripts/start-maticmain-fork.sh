fb20c4d1e166ade73744da64633e8bfdeb0c5411
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2021-09-14 10:10:00 -0300

    Updates Polygon endpoints for ganache-cli

--- scripts/start-maticmain-fork.sh
+++ scripts/start-maticmain-fork.sh
@@ -4,7 +4,8 @@ source .env
 
 CHAIN_ID=137
 GAS_LIMIT=281474976710655
-FORK_URL='https://rpc-mainnet.maticvigil.com/'
+FORK_URL='https://polygon-mainnet.infura.io/v3/'$INFURA_PROJECT_ID
+#FORK_URL='https://rpc-mainnet.maticvigil.com/'
 
 BALANCE=100000000000000000000000
 
--- scripts/start-matictest-fork.sh
+++ scripts/start-matictest-fork.sh
@@ -4,7 +4,8 @@ source .env
 
 CHAIN_ID=80001
 GAS_LIMIT=281474976710655
-FORK_URL='https://rpc-mumbai.maticvigil.com/'
+FORK_URL='https://polygon-mumbai.infura.io/v3/'$INFURA_PROJECT_ID
+#FORK_URL='https://rpc-mumbai.maticvigil.com/'
 
 BALANCE=100000000000000000000000
 
