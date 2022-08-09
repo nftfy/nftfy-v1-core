ed9ab0474786b9487e1fb7732e16907eb7c7fa13
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2022-08-09 11:48:21 -0300

    Improves safePromise

--- src/index.ts
+++ src/index.ts
@@ -5,20 +5,20 @@ import { fetchNft as fetchLooksrareNft } from './looksrare';
 
 export { NftData } from './common';
 
-async function safePromise<T>(promise: Promise<T | null>): Promise<T | null> {
+async function safePromise<T>(provider: string, promise: Promise<T | null>): Promise<T | null> {
   try {
     return await promise;
   } catch (e) {
-    console.log(e);
+    console.error(provider + ': ' + String(e));
   }
   return null;
 }
 
 export async function fetchNft(apiKey: string, collection: string, tokenId: bigint, network = 'mainnet', validate = false): Promise<NftData | null> {
   const [nft1, nft2, nft3] = await Promise.all([
-    safePromise(fetchOpenseaV2Nft(apiKey, collection, tokenId, network, validate)),
-    safePromise(fetchOpenseaV1Nft(apiKey, collection, tokenId, network, validate)),
-    safePromise(fetchLooksrareNft('', collection, tokenId, network, validate)),
+    safePromise('opensea-v2', fetchOpenseaV2Nft(apiKey, collection, tokenId, network, validate)),
+    safePromise('opensea-v1', fetchOpenseaV1Nft(apiKey, collection, tokenId, network, validate)),
+    safePromise('lookrare', fetchLooksrareNft('', collection, tokenId, network, validate)),
   ]);
   return nft1 || nft2 || nft3 || null;
 }
