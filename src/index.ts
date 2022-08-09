57455d888ab1cafad4de4c5379cbcd01c8c87c62
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2022-08-09 11:44:41 -0300

    Adds safePromise protection

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,20 @@ import { fetchNft as fetchLooksrareNft } from './looksrare';
 
 export { NftData } from './common';
 
+async function safePromise<T>(promise: Promise<T | null>): Promise<T | null> {
+  try {
+    return await promise;
+  } catch (e) {
+    console.log(e);
+  }
+  return null;
+}
+
 export async function fetchNft(apiKey: string, collection: string, tokenId: bigint, network = 'mainnet', validate = false): Promise<NftData | null> {
   const [nft1, nft2, nft3] = await Promise.all([
-    fetchOpenseaV2Nft(apiKey, collection, tokenId, network, validate),
-    fetchOpenseaV1Nft(apiKey, collection, tokenId, network, validate),
-    fetchLooksrareNft('', collection, tokenId, network, validate),
+    safePromise(fetchOpenseaV2Nft(apiKey, collection, tokenId, network, validate)),
+    safePromise(fetchOpenseaV1Nft(apiKey, collection, tokenId, network, validate)),
+    safePromise(fetchLooksrareNft('', collection, tokenId, network, validate)),
   ]);
   return nft1 || nft2 || nft3 || null;
 }
