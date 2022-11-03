4cbdd1aaff75ad3938c5e36fabaf9a15830f996e
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2022-11-02 22:56:02 -0300

    Updates external acquirer v2 address

--- src/common.ts
+++ src/common.ts
@@ -1,5 +1,5 @@
 export const EXTERNAL_ACQUIRER = '0x8eD69629B8fA69eEf1b019a3a427C08DC24Dd35f';
-export const EXTERNAL_ACQUIRER_V2 = '0x0E59da47900cC881cB39A15A090cB0cD15cCCae1'; // goerli
+export const EXTERNAL_ACQUIRER_V2 = '0x2eE998A0B0c7D8427b20843F2c9bd832C3036691'; // goerli
 
 export type NftData = {
   collection: string;
